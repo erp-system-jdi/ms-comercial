bad7574325e4f8e82f39e58bbf67a37deca161d2
Giovanni Marchi <giovanni.marchi92@gmail.com>
2023-02-13 21:23:49 -0300

    new code

---
+++ src/main/java/br/com/erpsystem/mscomercial/MsComercialApplication.java
@@ -0,0 +1,13 @@
+package br.com.erpsystem.mscomercial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsComercialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsComercialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/erpsystem/mscomercial/MsComercialApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.erpsystem.mscomercial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsComercialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
