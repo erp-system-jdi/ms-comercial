89f501541b827db7a399fa9df3ecb07814b895a9
Giovanni Marchi <giovanni.marchi92@gmail.com>
2023-02-23 20:53:53 -0300

    paradas

---
+++ src/main/java/br/com/erpsystem/mscomercial/dataprovider/ClienteDataprovider.java
@@ -0,0 +1,4 @@
+package br.com.erpsystem.mscomercial.dataprovider;
+
+public class ClienteDataprovider {
+}
---
+++ src/main/java/br/com/erpsystem/mscomercial/entrypoint/ClienteEntrypoint.java
@@ -0,0 +1,4 @@
+package br.com.erpsystem.mscomercial.entrypoint;
+
+public class ClienteEntrypoint {
+}
